3dcceede6441b77a210b7089e445f75b60ea7292
Your Name <you@example.com>
2021-09-11 05:15:36 -0700

    My first file

---
+++ Rokon.py
@@ -0,0 +1,13 @@
+#Variable Declaration System
+student_name='Md.Rokonuzzaman'
+student_age=24
+student_registration_no=2020110019
+Diploma_GPA='3.50 out of 4:00'
+
+
+print("My name is",student_name)
+print(student_age)
+print(student_registration_no)
+print(Diploma_GPA)
+
+
